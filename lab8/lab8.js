c8a031d82f7bc8595211f1fdeef54eeeefcc55f9
Иван Осягин <ayecantry@gmail.com>
2024-05-27 17:09:51 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -4,18 +4,27 @@ function showDate() {
     out.innerHTML = 'Русская локаль: ' + today.toLocaleString('ru-RU');
 
     let out1 = document.getElementById('current-date1');
-    let today1 = new Date();
-    out1.innerHTML = 'Китайская локаль:' + today1.toLocaleString('zh-CN');
+    out1.innerHTML = 'Китайская локаль:' + today.toLocaleString('zh-CN');
 
     let out2 = document.getElementById('current-date2');
-    let today2 = new Date();
-    out2.innerHTML = 'Арабская локаль: ' + today2.toLocaleString('ar');
+    out2.innerHTML = 'Арабская локаль: ' + today.toLocaleString('ar');
 
     let out3= document.getElementById('current-date3');
-    let today3 = new Date();
-    out3.innerHTML = 'Тайская локаль: ' + today3.toLocaleString('th');
+    out3.innerHTML = 'Тайская локаль: ' + today.toLocaleString('th');
 
     let out4 = document.getElementById('current-date4');
-    let today4 = new Date();
-    out4.innerHTML = 'Египетская локаль: ' + today4.toLocaleString('ar-EG');
+    out4.innerHTML = 'Египетская локаль: ' + today.toLocaleString('ar-EG');
+
+    let year = today.getFullYear();
+    let mounth = today.getMonth();
+    let date = today.getDate();
+    let day = today.getDay();
+
+    let mounthArr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    let dayArr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+    document.getElementById('year').innerHTML = 'Сегодняшний год: ' + year;
+    document.getElementById('mounth').innerHTML = 'Сегодняшний месяц: ' + mounthArr[mounth];
+    document.getElementById('day').innerHTML = 'Сегодняшний день: ' + date;
+    document.getElementById('dayWeek').innerHTML = 'Сегодняшний день недели: ' + dayArr[day];
 }
